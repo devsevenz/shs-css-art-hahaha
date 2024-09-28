1a6e350dfa10405b0b707b948de3c4416b2f0aad
Jeremy Abante <158697910+devsevenz@users.noreply.github.com>
2024-09-28 23:23:44 +0800

    Add files via upload

---
+++ finalexam.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="finalexamstylesheet1.css">
+    <title>KILLER LEAGUE</title>
+</head>
+<body>
+ <div class="base">
+  <div class="container">
+    <div class="large-tomoe">
+        <div class="ltc"></div>
+        <div class="ltc2"></div>
+        <div class="lt1"></div>
+        <div class="lt2"></div>
+        <div class="lt3"></div>
+        <div class="lt4"></div>
+        <div class="lt5"></div>
+        <div class="lt6"></div>
+    </div>
+    <div class="large-tomoe2">
+     <div class="right-tomoe"></div>
+     <div class="rtc"></div>
+        <div class="rtc2"></div>
+        <div class="rt1"></div>
+        <div class="rt2"></div>
+        <div class="rt3"></div>
+        <div class="rt4"></div>
+        <div class="rt5"></div>
+        <div class="rt6"></div>
+    </div>
+ <div class="large-tomoe3">
+        <div class="ttc"></div>
+        <div class="ttc2"></div>
+        <div class="tt1"></div>
+        <div class="tt2"></div>
+        <div class="tt3"></div>
+        <div class="tt4"></div>
+        <div class="tt5"></div>
+        <div class="tt6"></div>
+ </div>
+    <div class="small-tomoe">
+        <div class="st1"></div>
+        <div class="st2"></div>
+        <div class="st3"></div>
+        <div class="st4"></div>
+    </div>
+</div>
+ </div>
+</body>
+</html>
